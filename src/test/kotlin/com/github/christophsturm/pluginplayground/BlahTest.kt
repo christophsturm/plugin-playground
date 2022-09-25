590833e75f6f4af1e3ef44c67b63b3af866e2e70
Christoph Sturm <me@christophsturm.com>
2022-09-25 13:25:41 +0200

    convert test to kotlin

--- src/test/kotlin/com/github/christophsturm/pluginplayground/BlahTest.kt
+++ src/test/kotlin/com/github/christophsturm/pluginplayground/BlahTest.kt
@@ -1,29 +1,23 @@
 // Copyright 2000-2018 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-package com.github.christophsturm.pluginplayground;
+package com.github.christophsturm.pluginplayground
 
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.roots.ContentEntry;
-import com.intellij.openapi.roots.ModifiableRootModel;
-import com.intellij.testFramework.LightProjectDescriptor;
-import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor;
-import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase;
-import com.intellij.testFramework.fixtures.MavenDependencyUtil;
-import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.module.Module
+import com.intellij.openapi.roots.ContentEntry
+import com.intellij.openapi.roots.ModifiableRootModel
+import com.intellij.testFramework.LightProjectDescriptor
+import com.intellij.testFramework.fixtures.DefaultLightProjectDescriptor
+import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
+import com.intellij.testFramework.fixtures.MavenDependencyUtil
 
-public class BlahTest extends LightJavaCodeInsightFixtureTestCase {
-
-    @NotNull
-    @Override
-    protected LightProjectDescriptor getProjectDescriptor() {
-        return new DefaultLightProjectDescriptor() {
-            @Override
-            public void configureModule(@NotNull Module module, @NotNull ModifiableRootModel model, @NotNull ContentEntry contentEntry) {
-                super.configureModule(module, model, contentEntry);
-                MavenDependencyUtil.addFromMaven(model, "com.google.code.findbugs:jsr305:3.0.2");
+class BlahTest : LightJavaCodeInsightFixtureTestCase() {
+    override fun getProjectDescriptor(): LightProjectDescriptor {
+        return object : DefaultLightProjectDescriptor() {
+            override fun configureModule(module: Module, model: ModifiableRootModel, contentEntry: ContentEntry) {
+                super.configureModule(module, model, contentEntry)
+                MavenDependencyUtil.addFromMaven(model, "com.google.code.findbugs:jsr305:3.0.2")
             }
-        };
+        }
     }
 
-    public void testItWorks() {
-    }
+    fun testItWorks() {}
 }
