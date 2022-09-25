67660ce8842a1dfb2eff6d1f4ea02e5946903c80
GitHub Action <action@github.com>
2022-09-25 10:55:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/christophsturm/pluginplayground/services/MyProjectService.kt
+++ src/main/kotlin/com/github/christophsturm/pluginplayground/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.christophsturm.pluginplayground.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.christophsturm.pluginplayground.MyBundle
 
 class MyProjectService(project: Project) {
 
